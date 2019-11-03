3ac4b10a903fe286093347af7aea66fb26ec14f0
tanya - cube <taanya.cube@gmail.com>
2019-11-03 17:19:12 +0100

    feat: Add lang switcher

--- assets/index.js
+++ assets/index.js
@@ -1,9 +1,18 @@
-function renderKeyboard() {
-    var body = document.body;
+var body = document.body;
+var currentLang = 'en';
+var input = undefined;
 
-    var input = document.createElement('textarea');
+function renderInput() {
+    input = document.createElement('textarea');
     input.className = 'input';
     body.appendChild(input);
+}
+
+function renderKeyboard(lang) {
+    var prevKeyboard = document.getElementsByClassName('keyboard');
+    if (prevKeyboard.length) {
+        prevKeyboard[0].remove();
+    }
 
     var keyboard = document.createElement('div');
     keyboard.className = 'keyboard';
@@ -16,8 +25,15 @@ function renderKeyboard() {
         for (var j = 0; j < keyboardConfig[i].length; j++) {
             var cell = document.createElement('div');
             cell.className = 'cell';
-            cell.innerText = keyboardConfig[i][j].en.main;
+            cell.innerText = keyboardConfig[i][j][lang].main;
             cell.style.width = keyboardConfig[i][j].width + 'px';
+            cell.onclick = (function (cellConfig) {
+                return function () {
+                    oldText = input.innerHTML;
+                    input.innerHTML = oldText + cellConfig[lang].main;
+                }
+            })(keyboardConfig[i][j]);
+
             row.appendChild(cell);
         }
 
@@ -25,4 +41,28 @@ function renderKeyboard() {
     }
 }
 
-renderKeyboard();
+renderInput();
+renderKeyboard(currentLang);
+
+// cmd + shift
+var cmdDown = false;
+document.onkeydown = function (e) {
+    if (e.keyCode == 91)
+        cmdDown = true;
+
+    if (cmdDown && e.keyCode == 16) {
+        if (currentLang == 'en') {
+            currentLang = 'ru';
+        } else {
+            currentLang = 'en';
+        }
+
+        renderKeyboard(currentLang);
+    }
+};
+
+document.onkeyup = function (e) {
+    if (e.keyCode == 91) {
+        cmdDown = false;
+    }
+};
